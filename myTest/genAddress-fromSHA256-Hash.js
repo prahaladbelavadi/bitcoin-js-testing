e0ed66b8126a482187c7169ffea527b918059488
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-03-14 14:23:13 +0530

    display string, hash, address and buffer

--- myTest/genAddress-fromSHA256-Hash.js
+++ myTest/genAddress-fromSHA256-Hash.js
@@ -4,8 +4,8 @@ var bitcoin = require('../')
 var dhttp = require('dhttp/200')
 
 
-
-var hash = bitcoin.crypto.sha256(Buffer.from('correct horse battery staple'))
+var str = "correct horse battery staple"
+var hash = bitcoin.crypto.sha256(Buffer.from(str))
 var d = bigi.fromBuffer(hash)
+console.log("String:"+ str+ "\n" +"Hash:" + hash.toString('hex') + "\n" +"Address:" + d)
 console.log(bitcoin.crypto.sha256("correct horse battery staple"))
-console.log("Hash:" + hash.toString('hex') + "\n" +"Address:"+ d)

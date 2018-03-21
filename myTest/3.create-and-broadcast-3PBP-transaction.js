77c4364b2fc39dbdef45fc477c99114887cc4c46
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-03-21 12:19:21 -0400

    throws error saying callback is not a function

--- myTest/3.create-and-broadcast-3PBP-transaction.js
+++ myTest/3.create-and-broadcast-3PBP-transaction.js
@@ -19,11 +19,8 @@ var alice2Address = alice2.getAddress()
 console.log("Alice 1 Address: "+alice1Address+'\n'+"Alice 2 Address: "+alice2Address)
 
     regtestUtils.faucet(alice1Address, 5e4, function (err, unspent0) {
-      if (err) return (err)
 
       regtestUtils.faucet(alice2Address, 7e4, function (err, unspent1) {
-        if (err) return (err)
-
         var txb = new bitcoin.TransactionBuilder(regtest)
         txb.addInput(unspent0.txId, unspent0.vout) // alice1 unspent
         txb.addInput(unspent1.txId, unspent1.vout) // alice2 unspent
@@ -31,13 +28,13 @@ console.log("Alice 1 Address: "+alice1Address+'\n'+"Alice 2 Address: "+alice2Add
         txb.addOutput(aliceChange.getAddress(), 1e4) // Alice's change
         // (in)(4e4 + 2e4) - (out)(1e4 + 3e4) = (fee)2e4 = 20000, this is the miner fee
 
-        console.log(txb)
+        // console.log(txb)
         // Alice signs each input with the respective private keys
         txb.sign(0, alice1)
         txb.sign(1, alice2)
 
         // log built transaction
-        console.log(txb.build().toHex());
+        // console.log(txb.build().toHex());
 
         // build and broadcast our RegTest network
         regtestUtils.broadcast(txb.build().toHex())
